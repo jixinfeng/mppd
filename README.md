9f637fc3aa351571a233f333e24526419d2a0ce5
Jixin Feng <ufjfeng@users.noreply.github.com>
2019-02-22 23:14:42 -0500

    Modified code now it should works on more rss feeds

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A minimalistic python podcast downloader
 Aimed to download old NPR Planet Money podcasts provided by [this repo](https://github.com/xjcl/planetmoney-rss),
 will test on other feeds in the future
 
-Requires feedparser and mp3-tagger
+Requires feedparser
--- mppd.py
+++ mppd.py
@@ -3,70 +3,58 @@ import feedparser
 import pathlib
 import requests
 import time
-from mp3_tagger import MP3File
 import string
 
 
 class PodcastDownloader:
-    def __init__(self, rss, artist, album):
-        self.feed = feedparser.parse(rss)
-        self.album_info = dict()
-        self.album_info['artist'] = artist
-        self.album_info['album'] = album
-        self.dir_name = pathlib.Path("{} - {}".format(artist, album))
-        self.valid_chars = set("-_.() {}{}".format(string.ascii_letters, string.digits))
+    valid_chars = frozenset("-_.() {}{}".format(string.ascii_letters, string.digits))
+
+    def __init__(self, rss):
+        feed = feedparser.parse(rss)
+        self.author = feed.channel.author_detail.name
+        self.album = feed.channel.title
+        self.entries = self.parse_entries(feed)
+        self.dir_name = pathlib.Path("{} - {}".format(self.author, self.album))
         return
     
-    def download_all(self, n=None, reverse=False):
-        if n <= 0:
-            n = 1
-        if n > len(self.feed.entries):
-            n = len(self.feed.entries)
-        queue = []
-        if reverse:
-            for entry in self.feed.entries[::-1][:n]:
-                queue.append(self.entry_to_dict(entry))
-        else:
-            for entry in self.feed.entries[:n]:
-                queue.append(self.entry_to_dict(entry))
-
+    def download_all(self):
         self.dir_name.mkdir(exist_ok=True)
-        for i, q in enumerate(queue):
-            print("[{}]".format(i), end='')
-            self.download_and_process(q)
+        for entry in self.entries:
+            self.download_and_process(entry)
         
     def download_and_process(self, entry):
-        print("downloading {}".format(entry['title']))
-        old_name_path = self.dir_name / entry['old_name']
-        new_name_path = self.dir_name / entry['new_name']
+        print("[{}] downloading {}".format(entry['index'], entry['title']))
+        file_path = self.dir_name / entry['file_name']
+        tmp_path = file_path.with_suffix('.tmp')
         episode_link = entry['link']
-        if new_name_path.exists():
-            print("{} exists".format(new_name_path))
+        if file_path.exists():
+            print("{} exists".format(file_path))
             return
-        if old_name_path.exists():
-            old_name_path.unlink()
-        self.download_episode(episode_link, old_name_path)
-        self.update_tags(old_name_path, {**entry, **self.album_info})
-        old_name_path.rename(new_name_path)
+        if tmp_path.exists():
+            tmp_path.unlink()
+        self.download_episode(episode_link, tmp_path)
+        tmp_path.rename(file_path)
         return
-        
-    def entry_to_dict(self, entry):
-        link = entry['link']
-        filename = link.split('/')[-1].split('?')[0]
-        if '.' in filename:
-            ext_name = filename.split('.')[-1]
-        else:
-            ext_name = "mp3"
-        date = time.strftime("%Y%m%d", entry['published_parsed'])
-        title = entry['title']
-        return {
-            'link': link,
-            'date': date,
-            'title': title,
-            'old_name': filename,
-            'new_name': self.valid_filename("{} - {}.{}".format(date, title, ext_name))
-        }
-    
+
+    def parse_entries(self, feed):
+        entries = []
+        for index, entry in enumerate(feed.entries):
+            link = entry.links[0].href
+            published = entry['published_parsed']
+            year = published.tm_year
+            date = time.strftime("%Y%m%d", published)
+            title = entry['title']
+            filename = self.valid_filename("{} - {}.mp3".format(date, title))
+            entries.append({
+                'index': index,
+                'link': link,
+                'year': year,
+                'date': date,
+                'title': title,
+                'file_name': filename
+            })
+        return entries
+
     @staticmethod
     def download_episode(link, file_path):
         r = requests.get(link, stream=True) 
@@ -74,19 +62,11 @@ class PodcastDownloader:
             for chunk in r.iter_content(chunk_size=4*1024*1024): 
                 if chunk: 
                     f.write(chunk) 
-        return True
-    
+        return
+
     @staticmethod
-    def update_tags(file_path, tags):
-        file = MP3File(str(file_path))
-        file.album = tags['album']
-        file.artist = tags['artist']
-        file.song = tags['title']
-        file.save()
-        return True
-    
-    def valid_filename(self, filename):
-        return ''.join(c for c in filename if c in self.valid_chars)
+    def valid_filename(filename, valid_chars=valid_chars):
+        return ''.join(c for c in filename if c in valid_chars)
 
 
 def parse_args():
@@ -98,48 +78,17 @@ def parse_args():
         action='store',
         help="RSS Feed of podcast"
     )
-    parser.add_argument(
-        '--episodes', '-n',
-        type=int,
-        default=1,
-        action='store',
-        help="number of episodes to be downloaded, default 1"
-    )
-    parser.add_argument(
-        '--reverse',
-        action='store_true',
-        help="download from the oldest episode, selected by default"
-    )
-    parser.add_argument(
-        '--artist', '-a',
-        type=str,
-        help="artist of podcast"
-    )
-    parser.add_argument(
-        '--album_title', '-t',
-        metavar='album-title',
-        type=str,
-        help="album title of podcast"
-    )
     args = parser.parse_args()
-    parser.set_defaults(reverse=False)
-    if args.episodes <= 0:
-        parser.error("--episode must be bigger than 0")
 
     return args
 
 
 def main(args):
     d = PodcastDownloader(
-        rss=args.rss_feed,
-        artist=args.artist,
-        album=args.album_title
+        rss=args.rss_feed
     )
 
-    d.download_all(
-        n=args.episodes,
-        reverse=args.reverse
-    )
+    d.download_all()
 
 
 if __name__ == "__main__":
